e93bc78e1953b71deb75cdbd098a5284bc76b33d
thorny-rye <k.mytarev@bk.ru>
2024-07-19 13:14:45 +0300

    first

---
+++ main.py
@@ -0,0 +1,14 @@
+# 1st program
+print(9**0.5*5)
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+#3rd program
+print(1234 // 53 + 5678 // 84)
+#4th programm
+a = 13.42
+b =42.13
+ac=int(a)
+bc= int(b)
+ae= (round(int(a-ac)*100))
+be = round(int(b-bc)*100)
+print(ac==be or ae==be)
