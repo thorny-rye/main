42171a6f33dbd41abecc062cef76dee33b7b44f8
thorny-rye <k.mytarev@bk.ru>
2024-07-20 15:21:05 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,14 @@
+example = "Топинамбур"
+print(example[0])
+
+example = "Топинамбур"
+print(example[-1])
+
+example = "Топинамбур"
+print(example[5:])
+
+example = "Топинамбур"
+print(example[::-1])
+
+example = "Топинамбур"
+print(example[1:10:2])
