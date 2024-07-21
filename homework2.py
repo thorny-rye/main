3630788a85f5eac6276084c81c199381f1ef7930
thorny-rye <k.mytarev@bk.ru>
2024-07-21 19:56:23 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+homework_count = 12
+number_of_hours = 1.5
+name_of_curse = 'Python'
+time_on_a_one_task = number_of_hours/homework_count
+print("Курс: " + name_of_curse,"," "всего задач: " + str(homework_count),",""затрачено часов: " +
+ str(number_of_hours),"," "среднее время выполнения: " + str(time_on_a_one_task),"часа.")
