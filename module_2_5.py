c4ed4e94539a58b0f6b1652bb25d2cde95c3f2be
thorny-rye <k.mytarev@bk.ru>
2024-07-31 20:44:04 +0300

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -6,7 +6,7 @@ def get_matrix(n, m, value):
         for col in range(m):
             column = value
             strings.append(column)
-        return matrix
+    return matrix
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
