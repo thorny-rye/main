a529ac329843fffaf9d0f9110dce9e3c00e869f1
thorny-rye <k.mytarev@bk.ru>
2024-08-02 15:26:35 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,10 @@
+def password(n):
+    pairs= []
+    for i in range(1,21):
+        for j in range(i+1, 21):
+            if n % (i+j) == 0:
+                pairs.append(f'{i}{j}')
+    result = "".join(pairs)
+    return result
+for n in range(3,21):
+    print(f'{n}-{password(n)}')
