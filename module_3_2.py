5a056972da6c105ea9d749ae96bed250b1afedd1
thorny-rye <k.mytarev@bk.ru>
2024-08-05 20:32:27 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -18,7 +18,7 @@ def send_email(message, recipient, *, sender="university.help@gmail.com"):
             break
     return message, recipient, sender
 
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com'))
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com'))
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk'))
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru'))
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
